47b8696ae3efed87bd7903b7001e47b4e9127726
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-01-05 00:23:08 +0300

    NewsProject

---
+++ NewsProject2/NewsProject2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NewsProject2
+//
+//  Created by Eren Çetintaş on 5.01.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
