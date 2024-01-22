80961d34ae5db4d528f763e9c4d39dd56e9a1c7a
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-01-22 23:32:59 +0300

    Update ViewController.swift

--- AlertApplication/AlertApplication/ViewController.swift
+++ AlertApplication/AlertApplication/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,12 +26,46 @@ class ViewController: UIViewController {
             
         }
         alertcontroller.addAction(alertactioncancel)
-       
+        
         let alertactionok = UIAlertAction(title: "ok", style: .destructive) {
             action in
             print("ok")
+            
         }
         alertcontroller.addAction(alertactionok)
-        self.present(alertcontroller, animated: true, completion: nil)
+        self.present(alertcontroller, animated: true)
+    }
+    
+    
+    @IBAction func Alert2(_ sender: Any) {
+        
+        let alertcontroller2 = UIAlertController(title: "Title", message: "Alert2", preferredStyle: .alert)
+        
+        alertcontroller2.addTextField {
+            textfield in
+            
+            textfield.placeholder = "email"
+            textfield.keyboardType = .emailAddress
+            
+        }
+        alertcontroller2.addTextField {
+            textfield in
+            
+            textfield.placeholder = "password"
+            textfield.isSecureTextEntry = true
+            
+            
+        }
+        let action = UIAlertAction(title: "save", style: .default){ action1 in
+            
+            
+            let getemail = (alertcontroller2.textFields![0] as UITextField).text!
+            let getpassword = (alertcontroller2.textFields![1] as UITextField).text!
+            
+            self.Label.text = "Email:\(getemail)-Password:\(getpassword)"
+        }
+        alertcontroller2.addAction(action)
+        self.present(alertcontroller2, animated: true, completion: nil)
+        
     }
 }
