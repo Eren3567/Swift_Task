e39676019b73c10c6aa8c94727812fac9ad50e9d
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-01-28 11:59:01 +0300

    CollectionView

---
+++ CollectionView/CollectionView/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Eren Çetintaş on 28.01.2024.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var Celllabel: UILabel!
+    
+}
