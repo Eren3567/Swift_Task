daadf5b6b6cb9a72560898e1f3691499ccf0e165
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-02-03 21:53:49 +0300

    Update ViewController.swift

--- AllowInApp/AllowInApp/ViewController.swift
+++ AllowInApp/AllowInApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound], completionHandler: { (granted,error) in
-            
+            UNUserNotificationCenter.current().delegate = self
             self.allowControl = granted
             
             if granted {
@@ -28,5 +28,25 @@ class ViewController: UIViewController {
         
         
     }
-    
+    @IBAction func NotificationButton1(_ sender: Any) {
+        
+        if allowControl{
+            let contents1 = UNMutableNotificationContent()
+            contents1.title = "title1"
+            contents1.subtitle = "title2"
+            contents1.body = "Message"
+            contents1.badge = 1
+            contents1.sound = UNNotificationSound.default
+            
+            let triggerr = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            let requestNotification  = UNNotificationRequest(identifier: "Notification", content: contents1, trigger: triggerr)
+            UNUserNotificationCenter.current().add(requestNotification, withCompletionHandler: nil)
+        }
+        
+    }
+}
+extension ViewController: UNUserNotificationCenterDelegate{
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner,.badge,.sound])
+    }
 }
