f2b538392b65594e3d970538e41b045618e61011
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-02-03 20:41:02 +0300

    FireBaseRealTime

---
+++ FireBaseRealTime/FireBaseRealTime/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  FireBaseRealTime
+//
+//  Created by Eren Çetintaş on 3.02.2024.
+//
+
+import Foundation
+
+class Person {
+    
+    var ad : String?
+    var yas : Int?
+    
+    init(ad: String? = nil, yas: Int? = nil) {
+        self.ad = ad
+        self.yas = yas
+    }
+}
---
+++ FireBaseRealTime/FireBaseRealTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseRealTime
+//
+//  Created by Eren Çetintaş on 3.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var ref : datab
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
