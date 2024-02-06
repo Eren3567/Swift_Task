cd2547fecf0bd00bc89ad9f0df595cea423260d0
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-02-06 21:51:11 +0300

    Update ViewController2.swift

--- CaseStudy1/CaseStudy1/ViewController2.swift
+++ CaseStudy1/CaseStudy1/ViewController2.swift
@@ -10,7 +10,11 @@ import UIKit
 class ViewController2: UIViewController {
     var name = ""
     var titlename = ""
-    var imageimage: UIImage?
+    var imageimage = ""
+    var iconImageNames: [String] = [] // ViewController'dan alınacak resim adları
+    var isYellow = false
+    
+    @IBOutlet weak var iconimage2: UIImageView!
     
     @IBOutlet weak var Label4: UILabel!
     @IBOutlet weak var Label3: UILabel!
@@ -19,14 +23,71 @@ class ViewController2: UIViewController {
         super.viewDidLoad()
         Label3.text = name
         Label4.text = titlename
-        Image2.image = imageimage
+        Image2.image = UIImage(named: imageimage)
+      
+        iconimage2.image = UIImage(named: iconImageNames[0])
         
+        iconimage2.isUserInteractionEnabled = true
+        
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(iconImageTapped))
+                
+                iconimage2.addGestureRecognizer(tapGesture)
+        
+        let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(iconImageTapped2))
+        
+                Label3.addGestureRecognizer(tapGesture2)
+       
+        
+        if let imageUrl = URL(string: self.imageimage) {
+                    DispatchQueue.global().async {
+                        if let imageData = try? Data(contentsOf: imageUrl) {
+                            DispatchQueue.main.async {
+                                self.Image2.image = UIImage(data: imageData)
+                            }
+                        }
+                    }
+                }
+     /*  DispatchQueue.main.async{
+            if let imageURL = URL(string: self.imageimage),
+              let imageData = try? Data(contentsOf: imageURL),
+               let image = UIImage(data: imageData) {
+                self.Image2.image = image
+            }
+        }
+
+       */
     }
     
-    @IBAction func YellowButton(_ sender: UIButton) {
+    
+    
+    
+    
+    @objc func iconImageTapped() {
         
-        sender.tintColor = UIColor.yellow
-    }
+        print("click the icon")
+            if isYellow {
+                // Eğer sarı renk varsa, sarı rengi kaldır
+                iconimage2.tintColor = UIColor.clear
+                isYellow = false
+            } else {
+                // Sarı renk yoksa, sarı rengi ekle
+                iconimage2.tintColor = UIColor.yellow
+                isYellow = true
+            }
+        }
+    @objc func iconImageTapped2() {
+            if isYellow {
+                // Eğer sarı renk varsa, sarı rengi kaldır
+                Label3.tintColor = UIColor.clear
+                isYellow = false
+            } else {
+                // Sarı renk yoksa, sarı rengi ekle
+                Label3.tintColor = UIColor.yellow
+                isYellow = true
+            }
+        }
+    
 }
     /*
     // MARK: - Navigation
