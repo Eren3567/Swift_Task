e3dd24aa85829af209f89bc313fe9d94845ace08
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-02-11 15:18:49 +0300

    Update TableViewCell.swift

--- CaseStudy1/CaseStudy1/TableViewCell.swift
+++ CaseStudy1/CaseStudy1/TableViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseStorage
 protocol StarIconDelegate: AnyObject {
     func starIconTapped(at indexPath: IndexPath)
 }
@@ -29,5 +30,28 @@ class TableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    func setImageFromFirestore() {
+           let storage = Storage.storage() // Firebase Storage nesnesini oluştur
+           let storageRef = storage.reference() // Firebase Storage referansını al
+           
+           // Resmin bulunduğu referansı belirt
+           let imageRef = storageRef.child("images/image1.jpg")
+           
+           // Resmi indir
+           imageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+               if let error = error {
+                   print("Error downloading image: \(error.localizedDescription)")
+               } else {
+                   // İndirilen veriyi UIImage olarak oluştur
+                   if let imageData = data, let image = UIImage(data: imageData) {
+                       DispatchQueue.main.async {
+                           // UIImage'i kullanarak iconimage1 görünümüne ata
+                           self.iconimage1.image = image
+                       }
+                   } else {
+                       print("Error creating image from data.")
+                   }
+               }
+           }
+       }
 }
