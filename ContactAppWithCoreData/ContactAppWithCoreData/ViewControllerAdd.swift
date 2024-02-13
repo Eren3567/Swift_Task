dd658a7038699989e931698772115f8ba8d949a2
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-02-13 23:27:48 +0300

    Update ViewControllerAdd.swift

--- ContactAppWithCoreData/ContactAppWithCoreData/ViewControllerAdd.swift
+++ ContactAppWithCoreData/ContactAppWithCoreData/ViewControllerAdd.swift
@@ -7,29 +7,32 @@
 
 import UIKit
 
+let context1 = appdelegate.persistentContainer.viewContext
 class ViewControllerAdd: UIViewController {
-    @IBAction func contactName(_ sender: Any) {
-    }
+    @IBOutlet weak var contactName: UITextField!
+    @IBOutlet weak var contactNumber: UITextField!
+ 
     
-    @IBAction func ContactNumber(_ sender: Any) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func AddButton(_ sender: Any) {
+     print("bu buttona basıldı")
+        if let name = contactName.text, let phone = contactNumber.text {
+            let person = Contacts(context: context1)
+            person.phoneName = name
+            person.phoneNumber2 = phone
+            
+            appdelegate.saveContext()
+            
+          
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
+    
 }
+
