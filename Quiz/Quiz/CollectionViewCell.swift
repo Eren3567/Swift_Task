07e4122bc5c03a8cfde3d2711d02b612129a790e
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-02-24 20:40:18 +0300

    Create CollectionViewCell.swift

---
+++ Quiz/Quiz/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Quiz
+//
+//  Created by Eren Çetintaş on 24.02.2024.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var question2023Image: UIImageView!
+    
+    
+}
