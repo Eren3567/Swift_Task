0f84b5b4710862465c0af1528c3e1f7277b3502c
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-02-24 20:40:33 +0300

    Create ViewControllerFinished.swift

---
+++ Quiz/Quiz/ViewControllerFinished.swift
@@ -0,0 +1,33 @@
+//
+//  ViewControllerFinished.swift
+//  Quiz
+//
+//  Created by Eren Çetintaş on 18.02.2024.
+//
+
+import UIKit
+
+class ViewControllerFinished: UIViewController {
+
+    @IBOutlet weak var finishedLabel: UILabel!
+    @IBOutlet weak var finishedImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func completeLabel(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
