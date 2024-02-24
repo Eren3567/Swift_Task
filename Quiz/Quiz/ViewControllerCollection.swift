192e023bace1ab647953cbc2d5da25d91e64cfac
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-02-25 02:52:40 +0300

    Update ViewControllerCollection.swift

--- Quiz/Quiz/ViewControllerCollection.swift
+++ Quiz/Quiz/ViewControllerCollection.swift
@@ -4,48 +4,66 @@
 //
 //  Created by Eren Çetintaş on 24.02.2024.
 //
-/*
+
 import UIKit
 
 class ViewControllerCollection: UIViewController {
+    var collectionImages = [Collection]()
 
+    let k = Collection(collectionImage: "kpss")
+    let k2 = Collection(collectionImage: "main2")
     @IBOutlet weak var collectionQuestion: UICollectionView!
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionQuestion.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "mainCell")
-
+        
+        collectionImages.append(k)
+        collectionImages.append(k2)
+       
+        let layout = UICollectionViewFlowLayout()
+          layout.scrollDirection = .horizontal // Yatay kaydırma özelliğini etkinleştirin
+          collectionQuestion.collectionViewLayout = layout
+        
+        
         collectionQuestion.delegate = self
         collectionQuestion.dataSource = self
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
 extension ViewControllerCollection: UICollectionViewDelegate,UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return collectionImages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mainCell", for: indexPath) as! CollectionViewCell
-        cell.question2023Image.image = UIImage(named: "kpss")
+        
+        // IndexPath'e göre, dizideki ilgili resmin adını al
+              let imageName = collectionImages[indexPath.row]
+        cell.question2023Image.image = UIImage(named: imageName.collectionImage!)
+       
         return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+       
+        
+        performSegue(withIdentifier: "toQuestions", sender: nil)
         
     }
     
     
     
     
+ }
+extension ViewControllerCollection: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.bounds.size // Hücre boyutu koleksiyonun boyutuna eşit olmalı
+    }
     
-    
- }*/
+   
+}
