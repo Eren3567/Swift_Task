808a5bcf0afa0ed7e39e0652cee2b7d203301ae9
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-02-24 20:40:15 +0300

    Create Collection.swift

---
+++ Quiz/Quiz/Collection.swift
@@ -0,0 +1,22 @@
+//
+//  Collection.swift
+//  Quiz
+//
+//  Created by Eren Çetintaş on 21.02.2024.
+//
+
+import Foundation
+struct Collection {
+    
+    let collectionImage :String?
+    
+    let enterCollection :String?
+    
+    init(collectionImage: String?, enterCollection: String?) {
+        self.collectionImage = collectionImage
+        self.enterCollection = enterCollection
+    }
+ 
+
+    
+}
