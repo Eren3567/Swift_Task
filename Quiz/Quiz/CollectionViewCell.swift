78a7e65220e188f635d0659af8ee0dac17081c3c
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-03-25 22:19:20 +0300

    Update CollectionViewCell.swift

--- Quiz/Quiz/CollectionViewCell.swift
+++ Quiz/Quiz/CollectionViewCell.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
- 
+
     
     @IBOutlet weak var question2023Image: UIImageView!
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        question2023Image.layer.cornerRadius = 20
+                question2023Image.layer.masksToBounds = true // Köşeleri kesintiye uğrat
+    }
+
     
     
 }
