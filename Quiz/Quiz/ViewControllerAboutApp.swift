529bf52141f15d7f0de2082f00d2779b28b9dc20
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-03-25 22:19:02 +0300

    Create ViewControllerAboutApp.swift

---
+++ Quiz/Quiz/ViewControllerAboutApp.swift
@@ -0,0 +1,43 @@
+//
+//  ViewControllerAboutApp.swift
+//  Quiz
+//
+//  Created by Eren Çetintaş on 9.03.2024.
+//
+
+import UIKit
+
+class ViewControllerAboutApp: UIViewController {
+
+    @IBOutlet weak var versionLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+              
+              // Arka plan görüntüsünü ayarlar
+              backgroundImage.image = UIImage(named: "Home2")
+              
+              // Görüntünün arka planda görünmesini sağlar
+              backgroundImage.contentMode = .scaleAspectFill
+              
+              // Görüntüyü arka plana ekler
+              self.view.insertSubview(backgroundImage, at: 0)
+        
+        // Uygulama sürümünü alıp etiket üzerine yazdırma
+                if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+                    versionLabel.text = "Uygulama versiyonu " + version
+                }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
