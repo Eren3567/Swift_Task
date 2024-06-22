7ddcd8907e1e7e5e4e1ba86698988d3635fbce40
Eren3567 <149962472+Eren3567@users.noreply.github.com>
2024-06-22 23:31:41 +0300

    Create ViewController2.swift

---
+++ Table3/Table3/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  Table3
+//
+//  Created by Eren Çetintaş on 22.06.2024.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+var label2text = ""
+    @IBOutlet weak var label2: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label2.text = label2text
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
